f7a4f3818a200d5774d72beaac99b765aec55fe6
Antonio Acatecatl <antoniosud@hotmail.com>
2024-11-13 19:50:17 -0700

    adding new files

--- HTML/javascript/week1/JS-W1-HW.js
+++ HTML/javascript/week1/JS-W1-HW.js
@@ -1,29 +1,30 @@
 // Arrays of sentence fragments
 
 // Add an array of at least 5 Hero Adjectives
-const heroAdjectives = ["Clever", "Kind", "Combative", "Strong", "Brave"];
+const adjectives = ["Intrepid", "Fearless", "Combative", "Strong", "Brave", "Courageous", "Bold", "Dauntless", "Gutsy"];
 // Add an array of at least 5 Hero Nouns
-const heroNouns = ["Superman", "Black Widow", "Iron Man", "Spiderman", "Wonder Woman"];
+const nouns = ["Thor", "Black Widow", "Iron Man", "Spiderman", "Wonder Woman", "Hulk", "Doctor Strange", "Captain America", "Daredevil", "Aquaman", "Wolverine"];
 // Add an array of at least 5 Super Hero Powers
-const powers = ["persuasion", "super speed", "danger warning", "super intelligence", "heat vision"];
+const powers = ["fly", "super speed", "danger warning", "super intelligence", "heat vision", "claws", "X-ray vision", "telephaty", "invisibility", "energy projection", "invulnerability", "precognition"];
 // Add an array of at least 5 Super Hero Purpose/Mission Statements
-const missions = ["protect the city", "save the world", "fight injustice", "defend the weak", "seeking justice"];
+const missions = ["protect the world", "defeat criminals", "fight against injustice", "establish peace", "help the weak"];
 
 // Create a function to generate a random superhero name and description
-function generateSuperHero() {
-    const adjective = heroAdjectives[Math.floor(Math.random() * heroAdjectives.length)];
-    const noun = heroNouns[Math.floor(Math.random() * heroNouns.length)];    
+function generate() {
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];    
     const power = powers[Math.floor(Math.random() * powers.length)];
     const mission = missions[Math.floor(Math.random() * missions.length)];
 
 // Save the super hero name and description as variables
-    const heroName = `${adjective} ${noun}`;
-    const heroDescription = `This super hero has the power of ${power} and their mission is to ${mission}.`;
+    const hero = adjective.concat(" ",noun);
+    const start = "This superhero has the amazing power of "
+    const description = start.concat(power," and the mission to ",mission);
 
     //Display the super hero name and description inside the DOM
-    document.getElementById("hero-name").textContent = heroName;
-    document.getElementById("hero-description").textContent = heroDescription;
+    document.getElementById("hero-name").textContent = hero;
+    document.getElementById("hero-description").textContent = description;
 }
 
 // Add event listener to the button
-document.getElementById("generate-button").addEventListener("click", generateSuperHero);
+document.getElementById("generate-button").addEventListener("click", generate);

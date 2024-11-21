818f2b9551ff227a3363e42d8d8990bf3e8b85f7
Antonio Acatecatl <antoniosud@hotmail.com>
2024-11-20 18:16:50 -0700

    modificate javascript

--- HTML/javascript/week2/JS-W1.js
+++ HTML/javascript/week2/JS-W1.js
@@ -1,20 +1,19 @@
 //get the time
 function greet(){
 let greeting;
-let name=document.getElementById("fname").value;
+let name=prompt("Please enter your name");
 let hrs=new Date().getHours();
 let n=validateName(name);
 //validate the hour
 
-if (hrs < 12) {
-    greeting="Good Morning"+name+"! Thank you for reviewing my resume site.";
+if (hrs>=5 && hrs < 12) {
+    greeting="Good Morning "+name+"! Thank you for reviewing my resume site.";
 }
-else if (hrs<18){
-    greeting="Good Afternoon"+name+"! Thank you for reviewing my resume site.";
+else if (hrs>=12 && hrs<18){
+    greeting="Good Afternoon "+name+"! Thank you for reviewing my resume site.";
 }
 else{
     greeting="Good Evening "+name+"! Thank you for reviewing my resume site.";
-    //document.getElementById("time").innerHTML=greeting;
 }
 if (n==1){
     document.getElementById("time").innerHTML=greeting;
@@ -45,12 +44,3 @@ function validateName(name){
     return 1;
     }
 }
-
-/*
-if (isNaN(name)){
-    document.getElementById("time").innerHTML=greeting;
-}
-else {
-    greeting="Input not valid";
-    document.getElementById("time").innerHTML=greeting;
-}*/

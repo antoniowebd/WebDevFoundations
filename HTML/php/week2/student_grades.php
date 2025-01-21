3d903f7e138a3d13f0e7772b89b289e71378be03
antoniowebd <webdevepslcc@hotmail.com>
2025-01-20 20:10:04 -0700

    grade

--- HTML/php/week2/student_grades.php
+++ HTML/php/week2/student_grades.php
@@ -42,7 +42,6 @@ if (!isset($_SESSION['students'])) {
 
 global $total_students;
 global $total_sum;
-global $low;
 
 // Track total number of students & sum of grades
 
@@ -121,8 +120,7 @@ if (!empty($_SESSION['students'])) {
     echo "<h3>Statistics:</h3>";
     echo "Average Grade: " . round(calculateAverage($total_sum, $total_students),1) . "<br>";
     echo "Highest Grade: " . findHighestGrade($_SESSION['students']) . "<br>";
-    $low=findLowestGrade($_SESSION['students']);
-    echo '<div class="result";> Lowest Grade: </div>' . $low   . "<br>";
+    echo '<div class="result";> Lowest Grade: </div>' . findLowestGrade($_SESSION['students']) . "<br>";
 }
 ?>
 

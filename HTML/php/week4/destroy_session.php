4f96b341f73aef0c8836e63b9d12ae04025bf30d
antoniowebd <webdevepslcc@hotmail.com>
2025-02-05 18:40:51 -0700

    new file

---
+++ HTML/php/week4/destroy_session.php
@@ -0,0 +1,1 @@
+echo "<strong>Username is </strong>" . $_SESSION[""] . "<br>";
--- HTML/php/week4/get_session.php
+++ HTML/php/week4/get_session.php
@@ -6,6 +6,7 @@ session_start()
 <html>
 <body>
     <?php
+    //The values displayed were set on previous page
     echo "<strong>Username is </strong>" . $_SESSION["username"] . "<br>";
     echo "<strong>Email is </strong>" . $_SESSION["email"] . "<br>";
     ?>

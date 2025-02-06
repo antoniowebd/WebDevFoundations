8f0133e542f4d4534a1cf0645527d21593ead89d
antoniowebd <webdevepslcc@hotmail.com>
2025-02-05 18:54:16 -0700

    new file

--- HTML/php/week4/destroy_session.php
+++ HTML/php/week4/destroy_session.php
@@ -1,1 +1,19 @@
-echo "<strong>Username is </strong>" . $_SESSION[""] . "<br>";
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+session_unset();
+echo"Variables were removed<br>";
+
+// destroy the session
+session_destroy();
+echo"Session was destroyed<br>";
+?>
+
+</body>
+</html>

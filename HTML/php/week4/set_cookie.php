3f8f040df2455687ae502439f3dad42817cd9213
antoniowebd <webdevepslcc@hotmail.com>
2025-02-05 17:51:59 -0700

    strong

--- HTML/php/week4/set_cookie.php
+++ HTML/php/week4/set_cookie.php
@@ -12,7 +12,7 @@
        echo "Cookie named '" . $cookie_name . "' is not set!";
    } else {
        echo "Cookie '" . $cookie_name . "' is set!<br>";
-       echo "Value is: " . $_COOKIE[$cookie_name];
+       echo "<strong>Value is: </strong>" . $_COOKIE[$cookie_name];
    }
    ?>
    

d97406ba52b8043a188e13406c08002ab70413ac
antoniowebd <webdevepslcc@hotmail.com>
2025-02-10 19:13:12 -0700

    modify file

--- HTML/php/week5/x.php
+++ HTML/php/week5/x.php
@@ -1,15 +1,15 @@
 <?php
 
-try {
+try{
     //DB configuration
     $dsn = "mysql:host=mysql.kberrio.slccwebdev.com; dbname=kb_class";
     $username="kb_student";
     $password="@Tacos4Life@";
     //PDO connection
     $pdo= new PDO($dsn,$username,$password);
-    $pdo→setAttrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdosetAttrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
-} 
+}
 catch(PDOException $e){
-    echo "Connection failed: " . $e→getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }

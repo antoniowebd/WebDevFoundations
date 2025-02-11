ca74678a652c0d4e899550bbada936c60b86b748
antoniowebd <webdevepslcc@hotmail.com>
2025-02-10 18:59:01 -0700

    new files

---
+++ HTML/php/week5/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+    //DB configuration
+    $dsn = "mysql:host=mysql.kberrio.slccwebdev.com; dbname=kb_class";
+    $username="kb_student";
+    $password="@Tacos4Life@";
+    //PDO connection
+    $pdo= new PDO($dsn,$username,$password);
+    $pdo→setAttrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+} 
+catch(PDOException $e){
+    echo "Connection failed: " . $e→getMessage();
+}

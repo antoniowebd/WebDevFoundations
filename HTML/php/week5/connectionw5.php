91fe6f1fda9c13178f6f886a4692e76adf447cf9
antoniowebd <webdevepslcc@hotmail.com>
2025-02-12 19:00:36 -0700

    new files

---
+++ HTML/php/week5/connectionw5.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+    //DB configuration
+    $dsn = "mysql:host=php-mysql-exercisedb.slccwebdev.com; dbname=php-mysql-exercisedb";
+    $username="phpmysqlexercise";
+    $password="mysqlexercise";
+    //PDO connection
+    $pdo= new PDO($dsn,$username,$password);
+    $pdo→setAttrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+} 
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}

43279d7432299225b81d7719e0db4017197801e4
antoniowebd <webdevepslcc@hotmail.com>
2025-02-17 21:21:14 -0700

    last insert

--- HTML/php/week5/index.php
+++ HTML/php/week5/index.php
@@ -64,7 +64,7 @@
     $string=implode(", ", $interests);
     $insertProduct = "
         INSERT INTO responses (name, email, age, gender, country)
-        VALUES ('$name', '$email', $age, '$gender', '$country')";
+        VALUES ('$name', '$email', $age, '$gender', '$country', '$comments', '$string')";
               
     $pdo->exec($insertProduct);
 } else {

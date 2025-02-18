88e44b4ef06e6ccd0c791e5ce227be625768da10
antoniowebd <webdevepslcc@hotmail.com>
2025-02-17 21:31:23 -0700

    style

--- HTML/php/week5/index.php
+++ HTML/php/week5/index.php
@@ -23,7 +23,7 @@
         //PDO connection
         $pdo= new PDO($dsn,$username,$password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected Successfully";
+        echo "Connected Successfully" . "<br";
     } 
     catch(PDOException $e){
         echo "Connection failed: " . $e->getMessage();
@@ -84,7 +84,7 @@
      $results = $pdo->query("SELECT * FROM responses");
 
      //Show everything we found
-     echo "<br> Here's what's in our store: <br>";
+     echo "<br> Here's the data contained in the table Responses: <br>";
      while($row = $results->fetch(PDO::FETCH_ASSOC)){
          echo "ID: " . $row['id'] .  
              " | Name: " . $row['name'] . 

bb9af656f314d635b68431130624142b1c608a72
antoniowebd <webdevepslcc@hotmail.com>
2025-02-20 21:45:33 -0700

    add file

--- HTML/php/week5/index.php
+++ HTML/php/week5/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Week 5 Homework: Survey Form Design</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Survey Form</h1>
